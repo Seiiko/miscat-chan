db1068467f145071b34c411f0625e94c257a4f8b
Seiko Eleanor <34883978+Seiiko@users.noreply.github.com>
2018-01-27 20:38:28 +0000

    ...

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ client.on("message", async message => { // Message handler event.
   }
 
   // UHM.
-  if (message.content.startsWith("I pratically hate Rich.")) {
+  if (message.content.startsWith("Rich")) {
 
     // Limit it to the bot owner.
     if(!message.member.roles.some(r=>["NSFW Goddess"].includes(r.name)) ) // If user doesn't have the Admin, Moderator or Bot Owner role.
